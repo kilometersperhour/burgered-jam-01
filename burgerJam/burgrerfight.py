4620f993971b941e2eaf4578a98ec9b4cd93c192
Miles Alasdair Martin <miles.martin@maine.edu>
2021-05-17 23:47:21 -0400

    Rev 1

--- burgerJam/burgrerfight.py
+++ burgerJam/burgrerfight.py
@@ -10,12 +10,26 @@ GREEN=(0,255,0)
 class Burger(object):
     def __init__(self):
         self.image = pygame.image.load("burgrer.png")
-        self.eyes = pygame.image.load("eyes_right.png")
+        #self.eyes = pygame.image.load("eyes_right.png")
+        self.eyes = [pygame.image.load("eyes_right.png"),pygame.image.load("eyes_up.png"),pygame.image.load("eyes_left.png"),pygame.image.load("eyes_down.png")]
+        #self.eyes[1] = pygame.image.load("eyes_up.png")
+        #self.eyes[2] = pygame.image.load("eyes_left.png")
+        #self.eyes[3] = pygame.image.load("eyes_down.png")
+
+        #self.hands[0] = pygame.image.load("arm_idle.png")
+        #self.hands[1] = pygame.image.load("arm_punched.png")
+        #self.hands[2] = pygame.image.load("arm_punching.png")
+        self.hands = [pygame.image.load("arm_idle.png",),  pygame.image.load("arm_punching.png"), pygame.image.load("arm_punched.png"), pygame.image.load("arm_idle.png",)]
+        self.handIndexL = 0
+        self.handIndexR = 0
         center1 = 100
         center2 = 200
         self.center = [center1, center2]
         self.centereyes = [center1+35, center2+30]
+        self.centerRightArm = [center1+60 , center2+45]
+        self.centerLeftArm = [center1+60 , center2+35]
         self.health = 100
+
 
     def move(self, x, y):
         self.center[0] += x
@@ -23,26 +37,56 @@ class Burger(object):
         self.center[1] += y
         self.centereyes[1] += y
 
+        self.centerLeftArm[0] += x
+        self.centerLeftArm[1] += y
+
+        self.centerRightArm[0] += x
+        self.centerRightArm[1] += y
 
     def draw(self, surf):
         #surf.blit(self.eyes, self.centereyes)
+        #surf.blit(self.eyes, self.centereyes)
+        surf.blit(self.hands[self.handIndexL], self.centerLeftArm)
         surf.blit(self.image, self.center)
-        surf.blit(self.eyes, self.centereyes)
+        surf.blit(self.eyes[0], self.centereyes)
+        surf.blit(self.hands[self.handIndexR], self.centerRightArm)
+
+
+
     def hit(self):
         self.health = self.health - 10
+
+    def punchLeft(self):
+        for i in range(2):
+            self.handIndexL +=1
+        self.handIndexL = 0
+    def punchRight(self):
+        for i in range(2):
+            self.handIndexR +=1
+        self.handIndexR = 0
+    #def block(self):
+
+
 
 class BurgerEnemy(object):
     def __init__(self):
         self.image = pygame.image.load("burgrer.png")
-        self.center = [300, 200]
+        self.eyes = pygame.image.load("eyes_left.png")
+        center1 = 1000
+        center2 = 500
+        self.center = [center1, center2]
+        self.centereyes = [center1+35, center2+30]
         self.health = 100
 
     def move(self, x, y):
         self.center[0] += x
+        self.centereyes[1] += x
         self.center[1] += y
+        self.centereyes[1] += y
 
     def draw(self, surf):
         surf.blit(self.image, self.center)
+        surf.blit(self.eyes, self.centereyes)
 
 
 class game(object):
@@ -60,6 +104,10 @@ class game(object):
         self.clock = pygame.time.Clock()
         self.player = Burger()
         self.enemy = BurgerEnemy()
+
+        self.last = pygame.time.get_ticks()
+        self.cooldown = 300
+
 
     def draw(self, surf):
         surf.blit(self.bg, self.center)
@@ -87,7 +135,37 @@ class game(object):
             move_x = keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]
             move_y = keys[pygame.K_DOWN] - keys[pygame.K_UP]
             self.player.move(move_x * 5, move_y * 5)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(event.button)
+                if event.button == 1:
+
+                    #for i in range(2):
+                    self.player.handIndexL += 1
+
+                    if self.player.handIndexL == 3:
+                        self.player.handIndexL = 0
+
+
+
+
+                    #self.player.handIndexL = 0
+                    #self.player.punchLeft()
+                    #for i in range(2):
+
+                    #self.player.handIndexL += 2
+                    #self.player.handIndexL = 0
+
+
+                if event.button == 2:
+                    self.player.block()
+                if event.button == 3:
+                    self.player.handIndexR += 1
+
+                    if self.player.handIndexR == 3:
+                        self.player.handIndexR = 0
             self.draw(self.screen)
+
             self.drawHealth(self.screen)
             #self.screen.fill([255, 255, 255])
             self.player.draw(self.screen)
